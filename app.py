ad057c24faf183398ff07f81e38e8c9ab49685ed
JMK200611 <jeyamuthukumaran.s2024@vitstudent.ac.in>
2026-09-16 17:07:38 +0530

    commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 with open("report.txt", "w") as f:
     f.write("Application Report\n")
-    f.write("Total Users: 120\n")
-    f.write("Active Sessions: 45\n")
+    f.write("Total Users: 110\n")
+    f.write("Active Sessions: 35\n")
 
 print("Report generated.")
